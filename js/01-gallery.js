4426521b11ba2fb60bf5641bd3d0e30c8115aa28
ZoyaBukhantseva <zoyapashchenko87@gmail.com>
2022-11-13 00:48:39 +0300

    hw7.firsttry

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryRef = document.querySelector(".gallery")
+console.log(galleryRef)
+const galleryCard = createGalleryCard(galleryItems)
+galleryRef.insertAdjacentHTML('beforeend', galleryCard)
+
+function createGalleryCard(galleryItems) {
+   return galleryItems.map(({preview, original,  description}) => {
+        return `<div class="gallery__item">
+       <a class="gallery__link" href="${original} ">
+     <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`}).join('')
+  
+}
 console.log(galleryItems);
+    
+galleryRef.addEventListener('click', onGalleryClick)
+
+function onGalleryClick(evt) {
+     evt.preventDefault();
+     
+    if (!evt.target.classList.contains('gallery__image')) {
+        return
+    }
+    // console.log(evt.target.dataset.source);
+    const { src, alt } = evt.target;
+    const instance = basicLightbox.create(`<img src="${evt.target.dataset.source}" alt="${alt}"/>`)
+
+instance.show()
+
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector("ul.gallery")
 
+const galleryCard = createGalleryCard(galleryItems)
+galleryRef.insertAdjacentHTML('beforeend', galleryCard)
+
+function createGalleryCard(galleryItems) {
+   return galleryItems.map(({preview, original,  description}) => {
+       return `<li><a class="gallery__item" href="${original}">
+        <img class="gallery__image"
+        src="${preview}" 
+        alt="${description}" 
+        title="${description}" /></a></li>`
+   }).join('')
+  
+} 
+const gallery = new SimpleLightbox('ul.gallery a');
+ gallery.on('show.simplelightbox', function () {
+    gallery.options.captionDelay = 250;
+  })
 console.log(galleryItems);
